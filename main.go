32a65609a0d4b08118f0d24a1a60643fe5f7c26d
Henry Peeples <66916746+hankpeeples@users.noreply.github.com>
2023-11-29 22:21:47 -0600

    Small refactoring/code organization changes

--- main.go
+++ main.go
@@ -2,10 +2,46 @@
 package main
 
 import (
+	"flag"
 	"os"
-	"star-request/request"
+	"star-request/utils"
+
+	"github.com/pterm/pterm"
 )
 
+var (
+	configPath   *string
+	excludeFiles *string
+	files        []string
+)
+
+func init() {
+	pterm.EnableDebugMessages()
+
+	// get program arguments
+	configPath = flag.String("config", "", "Path to your request config directory. Can be relative or absolute path.")
+	excludeFiles = flag.String("exclude", "", "Filenames from given/config directory to exclude. Regex supported.")
+
+	flag.Parse()
+}
+
 func main() {
-	request.Execute(os.Args)
+
+	pterm.Debug.Println(flag.CommandLine.Args())
+
+	// If no args, skip exe
+	if len(os.Args) < 2 {
+		pterm.Debug.Println("Looking in current directory...")
+		dir, _ := os.Getwd()
+		// Find *.sr.[json,yaml] in current directory
+		files = utils.FindConfigFile(dir)
+	}
+
+	if *configPath != "" {
+		pterm.Debug.Println("Looking in user defined directory...")
+		// Find *.sr.[json,yaml] in given directory(s)
+		files = utils.FindConfigFile(*configPath)
+	}
+
+	pterm.Info.Println("Attempting to run requests found in:", files)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -2,28 +2,42 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"regexp"
 
 	"github.com/pterm/pterm"
 )
 
+// unknownArgs show the unknown args error message
+func unknownArgs(args []string, index int) {
+	fmt.Println("Unknown args:")
+	for i, arg := range args {
+		if i == index {
+			fmt.Printf("\t%v\n", arg)
+		}
+	}
+	os.Exit(1)
+}
+
 // FindConfigFile finds all request configs in the given directory, Either the current
 // dir or a config file/files via given args.
-func FindConfigFile(dir string) ([]string, error) {
+func FindConfigFile(dir string) []string {
 	filenames := []string{}
-	pterm.Info.Printf("Looking for config file in '%s'\n", dir)
+	pterm.Debug.Printf("Looking for config file in '%s'\n", dir)
 
 	// open directory for reading
 	thisDir, err := os.Open(dir)
 	if err != nil {
-		pterm.Error.Println("Could not open current directory ...", err)
+		pterm.Error.Println("Could not open current director:\n\t", err.Error())
+		os.Exit(17)
 	}
 
 	// read dir and get all files
 	files, err := thisDir.ReadDir(-1)
 	if err != nil {
-		pterm.Error.Println("Could not read current directory ...", err)
+		pterm.Error.Println("Could not read current directory:\n\t", err.Error())
+		os.Exit(18)
 	}
 
 	// get only filenames
@@ -31,10 +45,10 @@ func FindConfigFile(dir string) ([]string, error) {
 		filenames = append(filenames, file.Name())
 	}
 
-	return getFilePath(filenames), nil
+	return getFilePath(filenames)
 }
 
-// GetFilePath matches correct filenames
+// getFilePath matches correct filenames
 func getFilePath(files []string) []string {
 	found := []string{}
 
