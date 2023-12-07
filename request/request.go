f5722cecfb67db75402a5f7c76dab3a003da4c85
Henry Peeples <66916746+hankpeeples@users.noreply.github.com>
2023-12-06 23:35:44 -0600

    Update request base

--- request/request.go
+++ request/request.go
@@ -1,9 +1,31 @@
 // Package request is the main builder and runner of parsed requests
 package request
 
-import "github.com/pterm/pterm"
+import (
+	"star-request/utils"
+
+	"github.com/pterm/pterm"
+)
 
 // BuildRequest creates the base request parsed from the file
-func BuildRequest() {
-	pterm.Debug.Println("Build Request")
+func BuildRequest(requests utils.JSONRequests) {
+	for _, req := range requests.Requests {
+		switch req.Method {
+		case GET:
+			pterm.Debug.Println("GET request")
+			break
+		case POST:
+			pterm.Debug.Println("POST request")
+			break
+		case PUT:
+			pterm.Debug.Println("PUT request")
+			break
+		case DELETE:
+			pterm.Debug.Println("DELETE request")
+			break
+		default:
+			pterm.Error.Println("Unknown request method:", req.Method)
+			break
+		}
+	}
 }
