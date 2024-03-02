f2de7e4c99c93e3182de277c5cd78222ea76092e
Henry Peeples <hgpeeples48@gmail.com>
2024-03-02 02:39:01 -0600

    Add json initial json parsing test

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -9,5 +13,38 @@ func TestGetFilePath(t *testing.T) {
 	ret := getFilePath(files)
 	if ret[0] != "three.sr.yaml" {
 		t.Errorf("Result incorrect, got: %s, want: %s.", ret[0], files[2])
+	}
+}
+
+func TestJSONParse(t *testing.T) {
+	name := "Request Name"
+	type JSON struct {
+		Name        string `json:"name"`
+		URL         string `json:"URL"`
+		Method      string `json:"method"`
+		ContentType string `json:"content-type"`
+	}
+
+	var jsonStruct JSON
+
+	jsonFile, err := os.Open("/Users/hankpeeples/go/src/github.com/hankpeeples/StarRequest/example.sr.json")
+	if err != nil {
+		t.Errorf("Unable to open file: %v", err)
+	}
+	defer jsonFile.Close()
+
+	goodJSON, err := io.ReadAll(jsonFile)
+	if err != nil {
+		t.Errorf("Read JSON file error: %s", err)
+	}
+
+	err = json.Unmarshal(goodJSON, &jsonStruct)
+	if err != nil {
+		t.Errorf("Unmarshal error: %s", err)
+	}
+
+	fmt.Println(jsonStruct)
+	if jsonStruct.Name != name {
+		t.Errorf("got '%s', expected '%s'", jsonStruct.Name, name)
 	}
 }
